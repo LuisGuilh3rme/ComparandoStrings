3de397be4532c5ceb12f04051ac16a8b8415b889
Luis Guilherme <luislebert01@outlook.com>
2023-03-08 08:19:40 -0300

    Add project files.

---
+++ ComparandoStrings/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Informe o primeiro texto:");
+string texto1 = Console.ReadLine();
+Console.WriteLine("Informe o segundo texto:");
+string texto2 = Console.ReadLine();
+
+bool comparando1 = String.Equals(texto1, texto2);
+Console.WriteLine("{0} == {1}? {2}", texto1, texto2, comparando1);
+
+bool comparando2 = String.Equals(texto1, texto2, StringComparison.CurrentCultureIgnoreCase);
+Console.WriteLine("{0} == {1}? {2} (Ignorando maiusculas e minúsculas)", texto1, texto2, comparando2);
